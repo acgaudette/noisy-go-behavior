58d544e41a637af40afc075fe04df3510b490bc1
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-11-16 23:48:46 -0500

    Compute diff on prediction

--- action.go
+++ action.go
@@ -9,9 +9,10 @@ type action struct {
 	meta string
 }
 
+// Returns difference between action cost and entity state prediction
 func (this *action) diff(state []attribute) (result float64) {
 	for i := 0; i < len(this.cost); i++ {
-		result += math.Abs(this.cost[i] - state[i].value)
+		result += math.Abs(state[i].prediction - this.cost[i])
 	}
 
 	return
