9759a24e6e7adbe9d4b41234469dda358705d079
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-11-17 00:27:48 -0500

    Print state on update

--- entity.go
+++ entity.go
@@ -10,11 +10,20 @@ type entity struct {
 }
 
 func (this *entity) updateValues(values []float64) {
-	for i := 0; i < len(this.state); i++ {
+	for i := range this.state {
 		this.state[i].value = values[i]
+
+		// Debug
+		fmt.Printf(
+			"\t%s.%s = %g\n",
+			this.meta,
+			this.state[i].meta,
+			this.state[i].value,
+		)
 	}
 }
 
+// Render action
 func (this *entity) do(a action) {
 	fmt.Printf("%s: %s\n", this.meta, a.meta)
 }
