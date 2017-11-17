3bc7dff176d34538d90659213db8e39ee7cf1fd0
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-11-17 00:36:34 -0500

    Add DELTA_SCALE constant

---
+++ constants.go
@@ -0,0 +1,6 @@
+package main
+
+// Scale factor for tick input to noise function
+// Smaller values are smoother, but make state change more slowly
+// Higher values are jerky and erratic
+const DELTA_SCALE = 0.1
